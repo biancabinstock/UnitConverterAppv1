8cc18a7304490c779e193bd65dce6df50f9f4438
Bianca Binstock <bianca.pr@gmail.com>
2015-04-14 14:48:31 -0400

    Initial Commit

---
+++ UnitConvert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConvert
+//
+//  Created by Bianca Binstock on 2015-04-14.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitConvert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitConvert
+//
+//  Created by Bianca Binstock on 2015-04-14.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
